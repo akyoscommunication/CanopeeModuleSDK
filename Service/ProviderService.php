e95d7808505f085a4a0b395b5a9b3e1682fb1a16
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2024-01-24 15:51:57 +0100

    Fix proxy

--- Service/ProviderService.php
+++ Service/ProviderService.php
@@ -160,8 +160,8 @@ class ProviderService
             'urlAuthorize' => $this->moduleUrl.'authorize',
             'urlAccessToken' => $this->moduleUrl.'token',
             'urlResourceOwnerDetails' => $this->moduleUrl,
-            'proxy' => 'http://localhost:7080',
-            'verify' => false,
+//            'proxy' => 'http://localhost:7080',
+//            'verify' => false,
         ]);
 
         if(!$this->user) {
