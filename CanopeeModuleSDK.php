11d14c191b28767afb2bab871571ac1b825e5dcb
Johan JOULKVA <johan.akyos@gmail.com>
2024-06-20 16:48:57 +0200

    add table component

--- CanopeeModuleSDK.php
+++ CanopeeModuleSDK.php
@@ -3,6 +3,7 @@
 namespace Akyos\CanopeeModuleSDK;
 
 use Akyos\CanopeeModuleSDK\DependencyInjection\CanopeeModuleSDKExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -14,5 +15,19 @@ class CanopeeModuleSDK extends Bundle
             $this->extension = new CanopeeModuleSDKExtension();
         }
         return $this->extension;
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['KnpPaginatorBundle'])) {
+            $container->prependExtensionConfig('knp_paginator', [
+                'template' => [
+                    'pagination' => '@CanoppeModuleSDK/table/Pagination/sliding.html.twig',
+                    'sortable' => '@CanoppeModuleSDK/table/Pagination/sortable_link.html.twig',
+                ],
+            ]);
+        }
     }
 }
