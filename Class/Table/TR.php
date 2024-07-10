dbaaa566aba45309346cb167b8bec4535bf47b26
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2024-07-10 11:21:47 +0200

    Add TR in table trait

--- Class/Table/TR.php
+++ Class/Table/TR.php
@@ -4,9 +4,9 @@ namespace Akyos\CanopeeModuleSDK\Class\Table;
 
 class TR
 {
-    public iterable $ths;
+    public array $ths;
 
-    public function __construct(iterable $ths)
+    public function __construct(array $ths)
     {
         $this->ths = $ths;
     }
